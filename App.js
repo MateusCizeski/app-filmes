336e7dc9b0cb8e3c71c41083017624197bc115e6
Mateus Cizeski <mateuscizeski@gmail.com>
2024-07-12 21:56:58 -0300

    primeira implementação de app de filmes consumindo api

--- App.js
+++ App.js
@@ -1,20 +1,35 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { StyleSheet, View, FlatList } from 'react-native';
+
+import api from './src/services/api';
+import Filmes from './src/Filmes';
 
 export default function App() {
+  const [filmes, setFilmes] = useState([]);
+
+  useEffect(() => {
+    async function loadFilmes() {
+      const response = await api.get('r-api/?api=filmes');
+      const data = response.data;
+      setFilmes(data);
+    }
+
+    loadFilmes()
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <FlatList 
+        data={filmes}
+        keyExtractor={item => String(item.id)}
+        renderItem={ ({ item }) => <Filmes data={item}/>}
+      />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flex: 1
   },
 });
